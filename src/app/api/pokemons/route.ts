80efb2c03f09e405c94f15b65c9782e15611e5e0
hj7321 <oboki2001@naver.com>
2024-07-04 16:11:59 +0900

    feat: 151개의 포켓몬 데이터를 불러오는 route 파일 생성

--- src/app/api/pokemons/route.ts
+++ src/app/api/pokemons/route.ts
@@ -1,24 +1,32 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
 
+const TOTAL_POKEMON = 151;
+
 export const GET = async (request: Request) => {
-  const totalPokemon = 151;
-
   try {
-    const allPokemonData = [];
-    for (let i = 1; i <= totalPokemon; i++) {
-      const response = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon/${i}`
-      );
-      const speciesResponse = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon-species/${i}`
-      );
-      const koreanName = speciesResponse.data.names.find(
-        (name: any) => name.language.name === "ko"
-      );
-      allPokemonData.push({ ...response.data, korean_name: koreanName.name });
-    }
+    const allPokemonPromises = Array.from(
+      { length: TOTAL_POKEMON },
+      (_, index) => {
+        const id = index + 1;
+        return Promise.all([
+          axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`),
+          axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
+        ]);
+      }
+    );
 
+    const allPokemonResponses = await Promise.all(allPokemonPromises);
+
+    const allPokemonData = allPokemonResponses.map(
+      ([response, speciesResponse], index) => {
+        const koreanName = speciesResponse.data.names.find(
+          (name: any) => name.language.name === "ko"
+        );
+        return { ...response.data, korean_name: koreanName?.name || null };
+      }
+    );
+    console.log(allPokemonData);
     return NextResponse.json(allPokemonData);
   } catch (error) {
     return NextResponse.json({ error: "Failed to fetch data" });
