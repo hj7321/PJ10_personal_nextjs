5edca548976132217531766c59ade1f9fc20a7f2
hj7321 <oboki2001@naver.com>
2024-07-05 09:56:26 +0900

    feat: api handler 코드에 타입 지정

--- src/app/api/pokemons/route.ts
+++ src/app/api/pokemons/route.ts
@@ -1,32 +1,35 @@
 import { NextResponse } from "next/server";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { Pokemon, TypeKoreanName } from "@/types/pokemon.type";
 
-const TOTAL_POKEMON = 151;
+const TOTAL_POKEMON: number = 151;
 
 export const GET = async (request: Request) => {
   try {
-    const allPokemonPromises = Array.from(
-      { length: TOTAL_POKEMON },
-      (_, index) => {
-        const id = index + 1;
-        return Promise.all([
-          axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`),
-          axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
-        ]);
-      }
-    );
+    const allPokemonPromises: Promise<
+      [AxiosResponse<Pokemon, any>, AxiosResponse<Pokemon, any>]
+    >[] = Array.from({ length: TOTAL_POKEMON }, (_, index) => {
+      const id: number = index + 1;
+      return Promise.all([
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`),
+        axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
+      ]);
+    });
 
-    const allPokemonResponses = await Promise.all(allPokemonPromises);
+    const allPokemonResponses: [
+      AxiosResponse<Pokemon, any>,
+      AxiosResponse<Pokemon, any>
+    ][] = await Promise.all(allPokemonPromises);
 
-    const allPokemonData = allPokemonResponses.map(
+    const allPokemonData: Pokemon[] = allPokemonResponses.map(
       ([response, speciesResponse], index) => {
-        const koreanName = speciesResponse.data.names.find(
-          (name: any) => name.language.name === "ko"
-        );
+        const koreanName: TypeKoreanName | undefined =
+          speciesResponse.data.names.find(
+            (name: any) => name.language.name === "ko"
+          );
         return { ...response.data, korean_name: koreanName?.name || null };
       }
     );
-    console.log(allPokemonData);
     return NextResponse.json(allPokemonData);
   } catch (error) {
     return NextResponse.json({ error: "Failed to fetch data" });
