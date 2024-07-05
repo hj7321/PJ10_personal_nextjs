80a910221f490355794e941c14d6c3d01eeafcfa
hj7321 <oboki2001@naver.com>
2024-07-05 09:55:59 +0900

    feat: 메인 페이지에 Link 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>안녕</div>
+    <main className="flex min-h-screen items-center justify-center bg-black text-white text-5xl font-black">
+      <Link href="/pokemonList">포켓몬 도감 보러가기</Link>
     </main>
   );
 }
